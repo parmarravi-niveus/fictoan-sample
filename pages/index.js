21222f27e8c1be1d3fd5cedd6adeec398a24fb9c
Ravi Parmar <ravi.parmar@niveussolutions.com>
2022-04-14 11:38:28 +0530

    added project

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
 import styles from "../styles/Home.module.css";
-import { Text, Card, Button } from "fictoan-react";
+import { Text, Card, Button, CheckBox } from "fictoan-react";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Card
+      {/* <Card
         padding="micro"
         shape="rounded"
         bgColour="salmon-40"
         borderColour="salmon"
       >
         Test
-      </Card>
+      </Card> */}
+      <CheckBox
+        as="input"
+        id="checkbox-1"
+        value="checkbox-1"
+        name="checkbox-1"
+        label="Check me"
+      />
     </div>
   );
 }
