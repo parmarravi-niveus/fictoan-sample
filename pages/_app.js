ce66c13bbab0fbc61139557cf93d36c1d52bc7cc
Ravi Parmar <ravi.parmar@niveussolutions.com>
2022-04-14 12:20:33 +0530

    added theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "fictoan-react";
 import { WebsiteLightTheme } from "../styles/Website.light.theme";
-import { GlobalFonts } from "../assets/fonts/fonts";
 
 function MyApp({ Component, pageProps }) {
   return (
