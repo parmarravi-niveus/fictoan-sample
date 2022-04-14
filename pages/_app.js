65d6a156f2142901d15fa498d93ccc5b529b4d92
Ravi Parmar <ravi.parmar@niveussolutions.com>
2022-04-14 12:20:09 +0530

    added theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "fictoan-react";
+import { WebsiteLightTheme } from "../styles/Website.light.theme";
+import { GlobalFonts } from "../assets/fonts/fonts";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={WebsiteLightTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,6 @@ import { Text, Card, Button, CheckBox } from "fictoan-react";
 export default function Home() {
   return (
     <div className={styles.container}>
-      {/* <Card
-        padding="micro"
-        shape="rounded"
-        bgColour="salmon-40"
-        borderColour="salmon"
-      >
-        Test
-      </Card> */}
       <CheckBox
         as="input"
         id="checkbox-1"
